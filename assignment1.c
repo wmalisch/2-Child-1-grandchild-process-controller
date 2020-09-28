554fdd1f7f426992787b176b4680ba9022c37848
Will Malisch <malisch.will@gmail.com>
2020-09-28 13:44:58 -0400

    Pushing up some edits to test parent and child 1 output

--- assignment1.c
+++ assignment1.c
@@ -32,37 +32,39 @@ int main(int argc, char **argv)
 
 	// You must insert the following into your code (Replace zeros with the appropriate values/variables)
 
-	printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n", 0, 0);
-	printf("child_1 (PID %d) created child_1.1 (PID %d)\n", 0, 0);
-	printf("child_1 (PID %d) is now complete\n", 0);
-	printf("parent (PID %d) created child_2 (PID %d)\n", 0, 0);
-	printf("child_2 (PID %d) is calling an external program external_program.out and leaving child_2..\n", 0);
+
+//
+//	printf("parent (PID %d) created child_2 (PID %d)\n", 0, 0);
+//	printf("child_2 (PID %d) is calling an external program external_program.out and leaving child_2..\n", 0);
 	// Hint: You might find snprintf() helpful in this assignment
 
 
-    pid_t x, y, pid;
+    pid_t x, y, pid1, pid2;
 
     pid1=fork();
     pid2=fork();
-
-
-
     // Parent execution
-    if(pid>0)
+    if(pid1>0)
     {
         x = getpid();
-        printf("parent process (PID %d) created child_1 (PID %d) \n", x, 0);
-
+        printf("parent process (PID %d) created child_1 (PID %d) \n", x, pid1);
+        printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n", x, pid1);
+        wait(NULL);
     }
-
-
+    else if(pid1==0)
+    {
+        printf("child_1 (PID %d) created child_1.1 (PID %d)\n", pid1, pid2);
+        printf("child_1 (PID %d) is now complete\n", pid1);
+    }
     // Unsuccessful fork execution
-    if(pid<0)
+    else if(pid1<0)
     {
         printf("fork was unsuccessful");
     }
+    else
+    {
+        return 0;
+    }
 
-
-
-	return 0;
+    return 0;
 }
