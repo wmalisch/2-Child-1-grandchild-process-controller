aa78c6b4bc8ef1a937d127a07486b24d23d0ced4
Will Malisch <malisch.will@gmail.com>
2020-09-29 10:01:04 -0400

    First part is working alright, now adding the child 2 process and execl call

--- assignment1.c
+++ assignment1.c
@@ -30,8 +30,8 @@ int main(int argc, char **argv)
 		exit(0);
 	}
 
-
-    pid_t x, y, pid, pid2;
+    int status;
+    pid_t x, y, pid, pid2, pid3;
 
     pid=fork();
 
@@ -41,17 +41,30 @@ int main(int argc, char **argv)
 
     if(pid>0){
         x = getpid();
-        printf("parent process (PID %d) created child_1 (PID %d\n",x,pid);
+        printf("parent process (PID %d) created child_1 (PID %d)\n",x,pid);
         printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n", x, pid);
         wait(NULL);
-        printf("parent (PID %d) created child_2 (PID %d)\n", x, pid2);
+        pid2=fork();
+        printf("end of parent with pid %d\n",x);
+        //printf("parent (PID %d) created child_2 (PID %d)\n", x, pid2);
     }
 
     if(pid==0){
+        pid=fork();
         x = getpid();
         y = getppid();
         printf("child_1 (PID %d) created child _1.1 (PID %d)\n", x, pid2);
         printf("child_1 (PID %d) is now complete\n",x);
+        exit(1);
+    }
+
+    if(pid2==0){
+        x=getpid();
+        int p = getpid();
+        y=getppid();
+        printf("parent (PID %d) created child_2 (PID %d)\n",x,y);
+        printf("child_2 (PID %d) is calling an external program external_program.out and leaving child_2...");
+        status = execl("external_program.out","external_program.out",NULL);
     }
 
 
