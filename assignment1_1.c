79042c4c8de8774524c79ed238e483096395ac7b
Will Malisch <malisch.will@gmail.com>
2020-09-29 12:35:10 -0400

    Rough draft is almost complete. Everything works, except order of child creation. Program still creates child 2 before child 1.1

--- assignment1_1.c
+++ assignment1_1.c
@@ -31,25 +31,36 @@ int main(int argc, char **argv)
     }
 
     int status;
-    pid_t x, y, pid, pid2, pid3;
+    pid_t x, y, pid1,pid2;
 
-    pid=fork();
-    wait(NULL);
-    pid=fork();
-
-    if(pid<0){
+    pid1=fork();
+    pid2=fork();
+    if(pid1<0 || pid2<0){
         printf("fork unsuccessful\n");
     }
 
-    if(pid>0){
-
+    // Parent process
+    if(pid1>0 && pid2>0){
+        x=getpid();
+        printf("parent process (PID %d) created child_1 (PID %d)\n",x,pid1);
+        printf("parent (PID %d) is waiting for child_1 (PID %d) to complete before creating child_2\n",x,pid1);
+        wait(NULL);
+        printf("parent (PID %d) created child_2 (PID %d)\n",x,pid2);
     }
 
-    if(pid==0){
-
+    // First child
+    if(pid1==0 && pid2>0){
+        y=getppid();
+        x=getpid();
+        printf("child_1 (PID %d) created child_1.1 (PID %d)\n",x,pid2);
+        printf("child_1 (PID %d) is now complete\n",x);
     }
 
-
+    // Second child
+    if(pid1>0 && pid2==0){
+        x=getpid();
+        printf("child_2 (PID %d) is calling an external program external_program.out and leaving child_2...\n",x);
+    }
 
 
     return 0;
